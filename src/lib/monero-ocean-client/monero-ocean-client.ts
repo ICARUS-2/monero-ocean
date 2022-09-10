69ec641790c55eb473531266534bb41ffda1a69d
ICARUS-2 <ebriffett01@gmail.com>
2022-09-10 19:43:44 -0400

    Added try/catch for all network interactions

--- src/lib/monero-ocean-client/monero-ocean-client.ts
+++ src/lib/monero-ocean-client/monero-ocean-client.ts
@@ -17,170 +17,261 @@ export default class MoneroOceanClient implements IMoneroOceanClient
 {
     async getNetworkStats(): Promise<networkStatsModel | null> 
     {
-        let result: Response = await fetch(ApiRoutes.getNetworkStatsRoute());
-        
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return NetworkStatsModel.fromJson(json);
-        }
+            let result: Response = await fetch(ApiRoutes.getNetworkStatsRoute());
+            
+            if (result.ok)
+            {
+                let json = await result.json();
+                return NetworkStatsModel.fromJson(json);
+            }
 
-        return null;
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getPoolStats(): Promise<poolStatsModel | null> 
     {
-        let result: Response = await fetch(ApiRoutes.getPoolStatsRoute());
-
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return PoolStatsModel.fromJson(json);
-        }
+            let result: Response = await fetch(ApiRoutes.getPoolStatsRoute());
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return PoolStatsModel.fromJson(json);
+            }
+
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getMinerStats(address: string): Promise<MinerStatsModel | null> 
     {
-        let result = await fetch(ApiRoutes.getMinerStatsRoute(address));
-
-        if (result.ok)
+        try
         {
-            let json = await result.json()
-            return MinerStatsModel.fromJson(json)
-        }
+            let result = await fetch(ApiRoutes.getMinerStatsRoute(address));
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json()
+                return MinerStatsModel.fromJson(json)
+            }
+    
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getAllWorkers(address: string): Promise<AllWorkersModel | null> 
     {
-        let result = await fetch(ApiRoutes.getAllWorkersRoute(address));
-        
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return AllWorkersModel.fromJson(json);
-        }
+            let result = await fetch(ApiRoutes.getAllWorkersRoute(address));
         
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return AllWorkersModel.fromJson(json);
+            }
+            
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getUserInfo(address: string): Promise<UserModel | null> 
     {
-        let result = await fetch(ApiRoutes.getUserRoute(address))
-
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return UserModel.fromJson(json);
-        }
+            let result = await fetch(ApiRoutes.getUserRoute(address))
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return UserModel.fromJson(json);
+            }
+    
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getRecentMoneroBlocks(): Promise<RecentBlockDataModel[] | null> 
     {
-        let result = await fetch(ApiRoutes.getMoneroBlocksRoute());
-
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return RecentBlockDataModel.listFromJson(json);
-        }
+            let result = await fetch(ApiRoutes.getMoneroBlocksRoute());
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return RecentBlockDataModel.listFromJson(json);
+            }
+
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getRecentAltBlocks(): Promise<RecentBlockDataModel[] | null> 
     {
-        let result = await fetch(ApiRoutes.getAltBlocksRoute());
-
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return RecentBlockDataModel.listFromJson(json);
-        }
+            let result = await fetch(ApiRoutes.getAltBlocksRoute());
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return RecentBlockDataModel.listFromJson(json);
+            }
+
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getRecentAltBlocksByPort(port: string): Promise<RecentBlockDataModel[] | null> 
     {
-        let result = await fetch(ApiRoutes.getAltBlocksByPortRoute(port));
-
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return RecentBlockDataModel.listFromJson(json);
-        }
+            let result = await fetch(ApiRoutes.getAltBlocksByPortRoute(port));
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return RecentBlockDataModel.listFromJson(json);
+            }
+
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getTransactionsForUser(address: string): Promise<TransactionDataModel[] | null> 
     {
-        let result = await fetch(ApiRoutes.getTransactionReportRoute(address))
-
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return TransactionDataModel.listFromJson(json);
-        }
+            let result = await fetch(ApiRoutes.getTransactionReportRoute(address))
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return TransactionDataModel.listFromJson(json);
+            }
+
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getUserBlockPayments(address: string, pageNumber: number, count: number): Promise<UserBlockPaymentRecord[] | null> 
     {
-        let result = await fetch(ApiRoutes.getUserBlockPaymentsRoute(address, pageNumber, count));
-
-        if (result.ok)
+        try 
         {
-            let json = await result.json();
-            return UserBlockPaymentRecord.listFromJson(json);
-        }
+            let result = await fetch(ApiRoutes.getUserBlockPaymentsRoute(address, pageNumber, count));
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return UserBlockPaymentRecord.listFromJson(json);
+            }
+
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async updateUserPayoutThreshold(address: string, amount: number): Promise<boolean> 
     {
-        let result = await fetch(ApiRoutes.getUpdateThresholdRoute(), {
-            "headers": {
-              "content-type": "application/json",
-            },
-            "body": `{\"username\":\"${address}\",\"threshold\":${amount}}`,
-            "method": "POST",
-          });
+        try
+        {
+            let result = await fetch(ApiRoutes.getUpdateThresholdRoute(), {
+                "headers": {
+                "content-type": "application/json",
+                },
+                "body": `{\"username\":\"${address}\",\"threshold\":${amount}}`,
+                "method": "POST",
+            });
 
-        return result.ok;
+            return result.ok;
+        }
+        catch(err)
+        {
+            return false;
+        }
     }
 
     async getConnectedMinersChart(): Promise<ChartPointModel[] | null> 
     {
-        let result = await fetch(ApiRoutes.getConnectedMinersChartRoute());
-
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return ChartPointModel.listFromJson(json, "ts", "cn");
-        }
+            let result = await fetch(ApiRoutes.getConnectedMinersChartRoute());
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return ChartPointModel.listFromJson(json, "ts", "cn");
+            }
+
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }
 
     async getUserGlobalHashrateChart(address: string): Promise<ChartPointModel[] | null> 
     {
-        let result = await fetch(ApiRoutes.getUserGlobalHashrateChartRoute(address));
-
-        if (result.ok)
+        try
         {
-            let json = await result.json();
-            return ChartPointModel.listFromJson(json, "ts", "hs2")
-        }
+            let result = await fetch(ApiRoutes.getUserGlobalHashrateChartRoute(address));
 
-        return null;
+            if (result.ok)
+            {
+                let json = await result.json();
+                return ChartPointModel.listFromJson(json, "ts", "hs2")
+            }
+
+            return null;
+        }
+        catch(err)
+        {
+            return null;
+        }
     }   
 }
